4736775224e1fd3a5e7153ed73bd7341b9ec9018
rizkygusna <rizki.farid1998@gmail.com>
2021-05-04 07:45:34 +0700

    adding static query to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,28 @@
-import { Link } from 'gatsby'
-import React from 'react'
+import { Link, graphql, useStaticQuery } from "gatsby"
+import React from "react"
 
 export default function Navbar() {
-    return (
-       <nav>
-           <h1>Web Warrior</h1>
-           <div className="links">
-               <Link to="/">Home</Link>
-               <Link to="/about">About</Link>
-               <Link to="/projects">Projects</Link>
-           </div>
-       </nav>
-    )
+  //static query with graphql
+  const data = useStaticQuery(graphql`
+    query SiteInfo {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  const { title } = data.site.siteMetadata
+
+  return (
+    <nav>
+      <h1>{title}</h1>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/projects">Projects</Link>
+      </div>
+    </nav>
+  )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,7 @@ import Layout from "../components/Layout"
 import * as styles from "../styles/home.module.css"
 
 //gatsby automatically create data prop consist of data query from graphql
-export default function Home({ data }) {
-  const { title, description } = data.site.siteMetadata
-
+export default function Home() {
   return (
     <Layout>
       <section className={styles.header}>
@@ -23,22 +21,7 @@ export default function Home({ data }) {
           </Link>
         </div>
         <img src="/banner.png" alt="site banner" style={{ maxWidth: "100%" }} />
-        <p>
-          {title} - {description}
-        </p>
       </section>
     </Layout>
   )
 }
-
-//query site info from graphql
-export const query = graphql`
-  query HomePageQuery {
-    site {
-      siteMetadata {
-        title
-        description
-      }
-    }
-  }
-`
