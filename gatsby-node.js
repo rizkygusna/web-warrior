7240948c9828d0005b4565da53c29434c19249bb
rizkygusna <rizki.farid1998@gmail.com>
2021-10-12 12:38:24 +0700

    adding project details pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,9 @@
+//inclode path module from node.js
 const path = require(`path`)
 
+//export createPages module
 exports.createPages = async ({ graphql, actions }) => {
+  //extract data from response object
   const { data } = await graphql(`
     query Articles {
       allMarkdownRemark {
@@ -13,9 +16,12 @@ exports.createPages = async ({ graphql, actions }) => {
     }
   `)
 
+  //create template for each articles
   data.allMarkdownRemark.nodes.forEach(node => {
     actions.createPage({
+      //set path of article
       path: "/projects/" + node.frontmatter.slug,
+      //set absolute path of template
       component: path.resolve("./src/templates/project-details.js"),
       context: { slug: node.frontmatter.slug },
     })
--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,20 +1,51 @@
-import { GatsbyImage } from "gatsby-plugin-image"
+import { graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import React from "react"
 import Layout from "../components/Layout"
 import * as styles from "../styles/project-details.module.css"
 
-export default function ProjectDetails() {
+export default function ProjectDetails({ data }) {
+  const { html } = data.markdownRemark
+  const { title, stack } = data.markdownRemark.frontmatter
+  const featuredImg = getImage(
+    data.markdownRemark.frontmatter.featuredImg.childImageSharp.gatsbyImageData
+  )
+
   return (
     <Layout>
       <div className={styles.details}>
-        <h2>title</h2>
-        <h3>stack</h3>
+        <h2>{title}</h2>
+        <h3>{stack}</h3>
         <div className={styles.featured}>
-          {/* <GatsbyImage image={}></GatsbyImage> */}
+          <GatsbyImage image={featuredImg}></GatsbyImage>
         </div>
         {/* outputting inner HTML in div */}
-        {/* <div className={styles.html} dangerouslySetInnerHTML={} /> */}
+        <div
+          className={styles.html}
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
       </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query ProjectDetails($slug: String) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        stack
+        title
+        featuredImg {
+          childImageSharp {
+            gatsbyImageData(
+              layout: FULL_WIDTH
+              placeholder: BLURRED
+              formats: AUTO
+            )
+          }
+        }
+      }
+    }
+  }
+`
